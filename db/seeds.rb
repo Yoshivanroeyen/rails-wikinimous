22bf684b499bc28e2047c6a6db7164e37f4ac3d1
yoshi van roeyen <yoshivanroeyen@gmail.com>
2017-10-31 15:07:53 +0100

    Finished Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+puts "Deleting all articles"
+Article.destroy_all
+
+
+puts "Creating 15 articles"
+15.times do
+  art = Article.create(title: Faker::Book.title, content:
+Faker::Lorem.sentence)
+  puts "Created #{art.title}"
+end
+
+puts "Bye!"
